2f497ecaf8547cb1834068e19cd818fea7c6c05e
Ansygroup <livepm8@gmail.com>
2026-06-02 18:07:55 +0300

    add: app/rss.xml/route.js

---
+++ app/rss.xml/route.js
@@ -0,0 +1,31 @@
+import RSS from 'rss';
+import { getAllPosts } from '../../lib/posts';
+import { siteConfig } from '../../lib/config';
+
+export const dynamic = 'force-static';
+
+export async function GET() {
+  const feed = new RSS({
+    title: siteConfig.name,
+    description: siteConfig.tagline,
+    site_url: siteConfig.url,
+    feed_url: `${siteConfig.url}/rss.xml`,
+    language: 'en-US',
+    pubDate: new Date(),
+    ttl: 60,
+  });
+  getAllPosts().forEach((p) => {
+    feed.item({
+      title: p.title,
+      description: p.excerpt || p.description || '',
+      url: `${siteConfig.url}/posts/${p.slug}`,
+      guid: p.slug,
+      author: p.author || siteConfig.author,
+      date: p.date,
+      categories: [p.category, ...(p.tags || [])],
+    });
+  });
+  return new Response(feed.xml({ indent: true }), {
+    headers: { 'Content-Type': 'application/rss+xml; charset=utf-8', 'Cache-Control': 'public, max-age=3600, s-maxage=3600' },
+  });
+}
