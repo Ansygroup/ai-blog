570f9c1426649201eda248caf7fcc8ae368c279c
Ansy <ansy@ansygroup.com>
2026-06-10 18:36:07 +0300

    fix: add middleware callback to withAuth for pages config

--- middleware.js
+++ middleware.js
@@ -1,10 +1,15 @@
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth({
-  pages: {
-    signIn: '/admin/login',
+export default withAuth(
+  function middleware(req) {
+    // let the default redirect happen
   },
-});
+  {
+    pages: {
+      signIn: '/admin/login',
+    },
+  }
+);
 
 export const config = {
   matcher: ['/admin', '/admin/((?!login|api/).*)'],
